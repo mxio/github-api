fb69df018e913e384c0ac98ae75b875294726e70
mxio <maitongxiong@gmail.com>
2019-02-24 14:18:49 -0600

    added function to make a GET request and print results to the console

--- index.js
+++ index.js
@@ -5,6 +5,28 @@
 //"https://api.github.com/users/:username/repos"
 
 
+const apiURL = "https://api.github.com/users/";
+
+function displayRepos(responseJson) {
+    console.log(responseJson);
+}
+
+function getRepos(githubUser) {
+    const url = apiURL + githubUser + "/repos";
+    console.log(url);
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayRepos(responseJson))
+        .catch(error => {
+            //error message
+        })
+}
+
 function watchForm() {
     $('form').submit(function(event) {
         event.preventDefault();
